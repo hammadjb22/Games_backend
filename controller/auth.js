783f7ffc77c12b9f33cb5d525571d61efb099007
hammad <hammadjb22@gmail.com>
2024-12-03 04:35:23 +0500

    1

--- controller/auth.js
+++ controller/auth.js
@@ -246,6 +246,7 @@ exports.login = async (req, res) => {
 
     return res.status(200).json({ success: true, token, user });
   } catch (error) {
+    console.log(error)
     return res.status(500).json({ success: false, message:error });
   }
 };
